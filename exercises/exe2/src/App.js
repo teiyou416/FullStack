07b0881774c7d024f02f515bd4fb30e22d7dd23d
teiyou <teiyou416@gmail.com>
2024-03-24 04:33:41 +0800

    3.24

---
+++ exercises/exe2/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+const Alert = ({ name }) => window.alert("$(name) is already in the phonebook");
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const personObject = {
+      name: newName,
+    };
+    const checkPerson = persons.find(
+      (person) => person.name === personObject.name,
+    );
+    console.log(checkPerson);
+    if (checkPerson === personObject.name) {
+      Alert(personObject);
+    }
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
